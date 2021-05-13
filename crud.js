568f12f5a0464eec0bf79f6f4707ce25fec5169e
Ikenna Okolo <iykeokolo97@gmail.com>
2021-05-13 03:12:28 +0300

    Add files via upload

---
+++ crud.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const mongoose = require('mongoose')
+const connectionString = 'mongodb://localhost:27017/crudapp';
+app.use(express.json())
+
+mongoose.connect(connectionString,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+},(err) => {
+    if (err){
+        console.log(err)
+    }else{ console.log('connectiion sucessful')}
+})
+const crudSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    country: String,
+})
+const Crud = mongoose.model('Crud', crudSchema)
+//post request
+app.post('/crud', function (req, res){
+    const crud = req.body.crud;
+    Crud.create({
+        name: crud.name,
+        email: crud.email,
+        country: crud.country
+    }, (err, newCrud) => { 
+        if(err){
+            return res.status(500).json({message: err})
+        }else{
+            return res.status(200).json({message: "sucessful", newCrud})
+        
+        }
+    })
+})
+app.get('/crud/:id', (req, res)=> {
+    Crud.findById(req.params.id, (err , crud) =>{
+    if (err){
+        return res.status(500).json({message:err})
+   }
+    else if (!crud){
+        return res.status(404).json({message: "info not found"})
+
+        }
+    else{
+        return res.status(200).json({crud})
+    }
+})
+app.put('/crud/:id', (req, res) => {
+    Crud.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        email: req.body.email,
+        country:req.body.country
+    },(err,crud)=>{
+        if(err){
+            return res.status(500).json({message:err})
+        }else if(!crud){
+            return res.status(404).json({message:"info not found"})
+        }else{
+            crud.save((err, savedcrud)=>{
+                if(err){
+                    return res.status(400).json({message:err})
+
+                }else{
+                     return res.status(200).json({message: "updated"})
+                
+                }
+                
+                    
+                });
+            }
+
+})
+        
+
+})
+})
+app.delete('/crud/:id', (req,res)=>{
+    Crud.findByIdAndDelete(req.params.id,(err, crud)=>{
+        if(err){
+            return res.status(500).json({message:err})
+
+        }
+        else if(!crud){
+        return res.status(404).json({message:"info not found"})
+    }
+    else{
+        return res.status(200).json({message:"deleted"})
+    
+    }
+    })
+})
+app.listen(port,() => console.log('app listening on port ${port}'));
